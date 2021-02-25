8bfb7b027b9135a3065106c35c66071c5c56674a
Dawid Grych <grych.dawid@gmail.com>
2021-02-25 08:56:18 +0100

    fix from java :)

--- PdgEngine3D/Mesh.cs
+++ PdgEngine3D/Mesh.cs
@@ -40,7 +40,8 @@ namespace PdgEngine3D
                             case 'f':
                                 var data1 = line.Split(' ');
                                 var index = tris.Length;
-                                for(int i=0; i<(data1.Length-1); i++);{
+                                for(int i=0; i<(data1.Length-1); i++)
+                                {
                                     Array.Resize(ref tris, index +(i+1));
                                     var tri = new Triangle()
                                     {
